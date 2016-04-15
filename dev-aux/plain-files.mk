15c7533af00cd141913d5736355695f3be41ccc8
Quinn Grier <quinn.grier@gmail.com>
2016-04-14 22:37:00 -0500

    Add plain-files.mk
    The author of this commit has waived all copyright and
    related or neighboring rights to the extent permitted by
    law as described by the CC0 1.0 Universal Public Domain
    Dedication. You should have received a copy of the full
    dedication along with this commit, typically as a file
    named <CC0-1.0.txt>. If not, it may be available at
    <https://creativecommons.org/publicdomain/zero/1.0/>.

---
+++ dev-aux/plain-files.mk
@@ -0,0 +1,20 @@
+all : INSTALL
+
+## begin_rules
+
+INSTALL : INSTALL.bot
+INSTALL : INSTALL.texi
+INSTALL : doc/texi/Building-and-Installation.texi
+	gatbps INSTALL
+
+## end_rules
+
+#
+# The authors of this file have waived all copyright and
+# related or neighboring rights to the extent permitted by
+# law as described by the CC0 1.0 Universal Public Domain
+# Dedication. You should have received a copy of the full
+# dedication along with this file, typically as a file
+# named <CC0-1.0.txt>. If not, it may be available at
+# <https://creativecommons.org/publicdomain/zero/1.0/>.
+#
