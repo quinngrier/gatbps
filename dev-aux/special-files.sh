d620314edd665807b6631cfa9bcc0e06e3132dcb
Quinn Grier <quinn.grier@gmail.com>
2016-04-14 22:39:33 -0500

    Update special-files.sh
    The author of this commit has waived all copyright and
    related or neighboring rights to the extent permitted by
    law as described by the CC0 1.0 Universal Public Domain
    Dedication. You should have received a copy of the full
    dedication along with this commit, typically as a file
    named <CC0-1.0.txt>. If not, it may be available at
    <https://creativecommons.org/publicdomain/zero/1.0/>.

--- dev-aux/special-files.sh
+++ dev-aux/special-files.sh
@@ -3,4 +3,7 @@ gatbps \
   build-aux/docbook.am \
   build-aux/plaintext.am \
   build-aux/texinfo.css \
+  dev-aux/CC0-1.0-commit.txt \
+  dev-aux/CC0-1.0-file.txt \
+  dev-aux/Makefile.am.vim \
 ;
