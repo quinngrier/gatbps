83ad2e8d7a7f9e5b22ac2b3ac4c34d5da7877aab
Quinn Grier <quinn.grier@gmail.com>
2016-10-06 23:54:36 -0500

    Generate barrow.texi
    The author of this commit has waived all copyright and
    related or neighboring rights to the extent permitted by
    law as described by the CC0 1.0 Universal Public Domain
    Dedication. You should have received a copy of the full
    dedication along with this commit, typically as a file
    named <CC0-1.0.txt>. If not, it may be available at
    <https://creativecommons.org/publicdomain/zero/1.0/>.

--- dev-aux/special-files.mk
+++ dev-aux/special-files.mk
@@ -48,6 +48,7 @@ all:
 	$(GENERATE) 'doc/texi/CC0-1.0-copying.texi'
 	$(GENERATE) 'doc/texi/PACKAGEDATE.texi.in'
 	$(GENERATE) 'doc/texi/PACKAGEVERSION.texi.in'
+	$(GENERATE) 'doc/texi/barrow.texi'
 	$(GENERATE) 'doc/texi/bdots.texi'
 	$(GENERATE) 'doc/texi/bothcontents.texi'
 	$(GENERATE) 'doc/texi/dprm.texi'
