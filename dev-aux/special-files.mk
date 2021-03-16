1911d0754ef77e6f9821ee6fe28330a3d67b70d6
Quinn Grier <quinn@quinngrier.com>
2021-03-16 00:03:12 -0700

    Finish removing GATBPS_RECIPE_MARKER.am from here

--- dev-aux/special-files.mk
+++ dev-aux/special-files.mk
@@ -19,7 +19,6 @@ files = \
   build-aux/GATBPS_CONFIG_LATER_RULES.am \
   build-aux/GATBPS_CP_RULES.am \
   build-aux/GATBPS_M4_RULES.am \
-  build-aux/GATBPS_RECIPE_MARKER.am \
   build-aux/GATBPS_VPATH_SEARCH.am \
   build-aux/GATBPS_V_PAD.am \
   build-aux/LTCURRENT.sh \
